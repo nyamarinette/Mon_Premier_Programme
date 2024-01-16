8549105086a56aa08bb8bfa5349a3e22cd814bd7
nyamarinette <nyamarinette2020@gmail.com>
2024-01-16 13:55:51 +0200

    Les noms spécifiques

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
 /*
 
+je vous présente par la suite quelques noms spécifiques :
+
+            \n : retour a la ligne ou nouvelle ligne
+            \t : tabulation
+            \r : retour chariot
+            \b : retour arriere
+            \f : nouvelle page
+
+ */
+
+/*
+
     Ceci sont des commentaires sur plusieurs lignes.
     Merci
 */
 public class Main {
     public static void main(String[] args) {
         //Cette instruction va imprimer sur la sortie:
-        System.out.println("Bonjour tout le monde ! :");
+        System.out.println("\tBonjour tout le monde ! : ");
 
 
     }
