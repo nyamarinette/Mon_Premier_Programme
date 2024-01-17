abbfca26c802df5aabcc6eda8447b01a3a51bf8d
nyamarinette <nyamarinette2020@gmail.com>
2024-01-17 19:24:43 +0200

    variables_et_constantes

--- src/Main.java
+++ src/Main.java
@@ -11,15 +11,20 @@ je vous présente par la suite quelques noms spécifiques :
  */
 
 /*
+    Declarer une variable c'est choisir le type de variable qu'on veut.
+    lorsque nous voulons nommer une variable evitons de mettre les caracteres speciaux, les espaces.
+une declaration peut s'ecrire en :
+    camel case qui s'ecrit comme ceci ( uneVariableUnPeuLongue )
+    pascal case qui s'ecrit comme ceci ( UneVariable )
+    snake case qui s'ecrit comme ceci (une_Variable )
 
-    Ceci sont des commentaires sur plusieurs lignes.
-    Merci
+
 */
 public class Main {
     public static void main(String[] args) {
+        int ageUtilisateur = 26;
         //Cette instruction va imprimer sur la sortie:
-        System.out.println("\tBonjour tout le monde ! : ");
-
+        System.out.println("ageUtilisateur : ");
 
     }
 }
