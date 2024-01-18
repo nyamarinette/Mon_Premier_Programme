5d97fe9dfa541be6fa8d15af55c478821fd1723a
nyamarinette <nyamarinette2020@gmail.com>
2024-01-18 11:55:14 +0200

    first operateur

--- src/Main.java
+++ src/Main.java
@@ -11,20 +11,14 @@ je vous présente par la suite quelques noms spécifiques :
  */
 
 /*
-    Declarer une variable c'est choisir le type de variable qu'on veut.
-    lorsque nous voulons nommer une variable evitons de mettre les caracteres speciaux, les espaces.
-une declaration peut s'ecrire en :
-    camel case qui s'ecrit comme ceci ( uneVariableUnPeuLongue )
-    pascal case qui s'ecrit comme ceci ( UneVariable )
-    snake case qui s'ecrit comme ceci (une_Variable )
+   ici c'est juste un essaye d operateur
+   merci
 
 
 */
 public class Main {
     public static void main(String[] args) {
-        int ageUtilisateur = 26;
-        //Cette instruction va imprimer sur la sortie:
-        System.out.println("ageUtilisateur : ");
-
+        int age = 24;
+        System.out.println("age" );
     }
 }
